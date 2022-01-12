9ea950e59d2eebc595872dd6b2a9aa0f03f35d74
bdulude <bdulude@gmail.com>
2022-01-11 20:06:29 -0800

    Method Chaining

--- user_object/user.py
+++ user_object/user.py
@@ -10,39 +10,31 @@ class User:
     
     def make_deposit(self, amount):
         self.balance = self.balance + amount
+        return self
     
     def make_withdrawal(self, amount):
         self.balance = self.balance - amount
+        return self
 
     def display_user_balance(self):
         print(self.name + ": " + str(self.balance))
-        # return self.balance
+        return self
 
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 
 charles = User("Charles Von Ham", 1234)
 dillon = User("Dillon Smith", 2000)
 cobie = User("Cobie Colbert", 44444)
 
-charles.make_deposit(1000)
-charles.make_deposit(500)
-charles.make_deposit(2000)
-charles.make_withdrawal(300)
-charles.display_user_balance()
+charles.make_deposit(1000).make_deposit(500).make_deposit(2000).make_withdrawal(300).display_user_balance()
 
-dillon.make_deposit(400)
-dillon.make_deposit(2000)
-dillon.make_withdrawal(2500)
-dillon.display_user_balance()
+dillon.make_deposit(400).make_deposit(2000).make_withdrawal(2500).display_user_balance()
 
-cobie.make_deposit(15000)
-cobie.make_withdrawal(5000)
-cobie.make_withdrawal(200)
-cobie.make_withdrawal(1000)
-cobie.display_user_balance()
+cobie.make_deposit(15000).make_withdrawal(5000).make_withdrawal(200).make_withdrawal(1000).display_user_balance()
 
 charles.transfer_money(cobie, 300)
 charles.display_user_balance()
